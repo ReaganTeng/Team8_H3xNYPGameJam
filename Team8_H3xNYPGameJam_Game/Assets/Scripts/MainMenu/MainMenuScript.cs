a2ab17eeb518b5895dc1d74fdb5e64e0b9e87632
darius122 <88877663+darius122@users.noreply.github.com>
2023-10-10 21:17:07 +0800

    added tap to start text, added game manager, finish transition from main menu to settings, pause menu, game etc

--- Team8_H3xNYPGameJam_Game/Assets/Scripts/MainMenu/MainMenuScript.cs
+++ Team8_H3xNYPGameJam_Game/Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -6,36 +6,38 @@ using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
-    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private GameObject mainMenuPanel;
+    private CanvasGroup mainMenuCanvasGroup;
     [SerializeField] private ScaleTextScript scaleTextScript;
     [SerializeField] private TMP_Text startText;
     [SerializeField] private GameObject settingsPanel;
     [SerializeField] private GameObject settingsButton;
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
+    [SerializeField] private TMP_Text settingsText;
+    [SerializeField] private GameObject pauseButton;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    StartCoroutine(fadeOut());
-        //}
+        mainMenuCanvasGroup = mainMenuPanel.GetComponent<CanvasGroup>();
     }
     private IEnumerator fadeOut()
     {
-        scaleTextScript.StopScaling();
-        while (canvasGroup.alpha > 0f)
+        scaleTextScript.StopScaling();                      
+        while (mainMenuCanvasGroup.alpha > 0f)
         {
-            canvasGroup.alpha -= Time.deltaTime / 2;
+            mainMenuCanvasGroup.alpha -= Time.deltaTime / 2;
             yield return null;
         }
+        mainMenuPanel.SetActive(false);
+        pauseButton.SetActive(true);
+        GameManagerScript.gmInstance.gameState = GameManagerScript.GameStates.PLAYING;
+    }
+    public void StartGame()
+    {
+        StartCoroutine(fadeOut());
     }
     public void onSettingsButtonClicked()
     {
+        GameManagerScript.gmInstance.gameState = GameManagerScript.GameStates.SETTINGS;
         Color newColor = new Vector4(0, 0, 0, 0);
         startText.color = newColor;
         settingsButton.SetActive(false);
@@ -43,9 +45,25 @@ public class MainMenuScript : MonoBehaviour
     }
     public void onSettingsBackButtonClicked()
     {
-        Color newColor = new Vector4(0, 0, 0, 1);
-        startText.color = newColor;
+        if(GameManagerScript.gmInstance.gameState == GameManagerScript.GameStates.SETTINGS)
+        {
+            Color newColor = new Vector4(0, 0, 0, 1);
+            startText.color = newColor;
+            GameManagerScript.gmInstance.gameState = GameManagerScript.GameStates.START;
+        }
+        else if(GameManagerScript.gmInstance.gameState == GameManagerScript.GameStates.PAUSED)
+        {
+            pauseButton.SetActive(true);
+            GameManagerScript.gmInstance.gameState = GameManagerScript.GameStates.PLAYING;
+        }
         settingsButton.SetActive(true);
         settingsPanel.SetActive(false);
+    }
+    public void onPauseButtonClicked()
+    {
+        GameManagerScript.gmInstance.gameState = GameManagerScript.GameStates.PAUSED;
+        pauseButton.SetActive(false);
+        settingsText.text = "PAUSED";
+        settingsPanel.SetActive(true);
     }
 }
