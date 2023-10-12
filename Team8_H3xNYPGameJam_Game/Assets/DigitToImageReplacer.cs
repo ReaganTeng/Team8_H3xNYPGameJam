9cfd28f7f0bee3e1ac355c5293903058702996f7
ReaganTeng <yuzhe100903@gmail.com>
2023-10-12 23:18:26 +0800

    removed unwanted GOs in shopcanvas

--- Team8_H3xNYPGameJam_Game/Assets/DigitToImageReplacer.cs
+++ Team8_H3xNYPGameJam_Game/Assets/DigitToImageReplacer.cs
@@ -8,48 +8,71 @@ using System.Linq;
 
 public class DigitToImageReplacer : MonoBehaviour
 {
-    public RawImage rawImage; // Reference to the RawImage component
-    public string textToReplace = "12345"; // The text to replace
-    public Texture2D[] digitImages; // An array of digit images (0-9)
-    TextMeshProUGUI textComponent;  // Reference to the UI Text component
+    public GameObject background; // Reference to the background sprite or GameObject
+
+    public Sprite[] characterSprites; // An array of sprites to use for replacement
+    public int maxDigits = 5;
+
+    public TextMeshProUGUI textComponent; // Reference to the TextMeshProUGUI component
 
     private void Awake()
     {
         textComponent = GetComponent<TextMeshProUGUI>();
-        rawImage = GetComponentInChildren<RawImage>();
     }
 
-    public void Update()
+    private void Update()
     {
-        ReplaceCharacters();
+        //ReplaceDigitsWithSprites();
+        SetNumber(12);
     }
 
-    void ReplaceCharacters()
+
+
+    public void SetNumber(int number)
     {
-        if (rawImage != null && textComponent != null)
+        string numberString = number.ToString();
+
+        // Ensure the number of digits doesn't exceed the maximum
+        if (numberString.Length > maxDigits)
+            numberString = new string('9', maxDigits);
+
+        for (int i = 0; i < maxDigits; i++)
         {
-            string replacedText = "";
+            // Get the individual digit
+            int digit = i < numberString.Length ? int.Parse(numberString[i].ToString()) : 0;
 
-            foreach (char character in textToReplace)
-            {
-                if (char.IsDigit(character) && character >= '0' && character <= '9')
-                {
-                    int digit = (int)char.GetNumericValue(character);
-
-                    if (digit >= 0 && digit < digitImages.Length)
-                    {
-                        // Replace the character with the corresponding image
-                        Texture2D image = digitImages[digit];
-                        replacedText += $"<texture={image.GetInstanceID()}>";
-                    }
-                }
-                else
-                    replacedText += character.ToString(); // Keep non-digits as they are
-            }
-
-            // Update the Text component with the replaced text
-            textComponent.text = replacedText;
+            // Access the digit GameObject or sprite and set the appropriate sprite
+            GameObject digitObject = background.transform.GetChild(i).gameObject;
+            Image digitImage = digitObject.GetComponent<Image>();
+            digitImage.sprite = characterSprites[digit];
         }
+    }
+
+    public void ReplaceDigitsWithSprites()
+    {
+        //string originalText = textComponent.text;
+        string originalText = "12345";
+
+        for (int i = 0; i < originalText.Length; i++)
+        {
+            char character = originalText[i];
+
+            if (char.IsDigit(character))
+            {
+                int digit = character - '0';
+
+                if (digit >= 0 && digit < characterSprites.Length)
+                {
+                    // Replace the character with a sprite
+                    string spriteTag = $"<sprite={digit}>";
+                    originalText = originalText.Remove(i, 1).Insert(i, spriteTag);
+                    i += spriteTag.Length - 1; // Adjust the index based on the length of the added tag
+                }
+            }
+        }
+
+        // Update the TextMeshProUGUI component with the replaced text
+        textComponent.text = originalText;
     }
 
 }
