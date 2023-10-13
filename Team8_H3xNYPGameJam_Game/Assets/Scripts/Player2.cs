7dbc40a52499448ea1eb4e9f95db5f5aa45440a6
ReaganTeng <yuzhe100903@gmail.com>
2023-10-13 13:51:33 +0800

    uncommented a code

--- Team8_H3xNYPGameJam_Game/Assets/Scripts/Player2.cs
+++ Team8_H3xNYPGameJam_Game/Assets/Scripts/Player2.cs
@@ -66,7 +66,7 @@ public class Player2 : MonoBehaviour
     {
         TrueOGPos = transform.position;
         startLocation = transform.position;
-        //shopCanvas.gameObject.SetActive(false);
+        shopCanvas.gameObject.SetActive(false);
         enemiesDefeated = 0;
 
 
