3a795d2637ed0922cadc1d1b67c1ce035e3dff52
darius122 <88877663+darius122@users.noreply.github.com>
2023-10-13 15:17:18 +0800

    gameover

---
+++ Team8_H3xNYPGameJam_Game/Assets/GameOverScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> numbersList = new List<Sprite>();
+    [SerializeField] private Image defaultImg;
+    [SerializeField] private GameObject scoreImg;
+    [SerializeField] private GameObject highScoreImg;
+    private void OnEnable()
+    {
+        string scoreLength = Player2.instance.totalenemiesDefeated.ToString();
+        for(int i = 0; i < scoreLength.Length; i++)
+        {
+            int digit = int.Parse(scoreLength[i].ToString());
+            Image scoreDigit = Instantiate(defaultImg, scoreImg.transform);
+            scoreDigit.sprite = numbersList[digit];
+            scoreDigit.GetComponent<RectTransform>().sizeDelta = new Vector2(88, 96);
+        }
+    }
+    public void OnRestartButtonClicked()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
